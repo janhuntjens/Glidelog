43ac844291131178056d31b78379abb47977a611
Jan Huntjens <jan@huntjens.me>
2011-06-05 15:18:25 +0200

    begin charting for stats

--- app/models/flight.php
+++ app/models/flight.php
@@ -17,18 +17,25 @@ class Flight extends AppModel
 		
 		foreach($flights as $i => $flight)
 		{
+				//FLIGHTTYPES
 				$stats['Flighttypes'][$flight['Flighttype']['title']]['numbers'] = $stats['Flighttypes'][$flight['Flighttype']['title']]['numbers']+1;
 				$stats['Flighttypes'][$flight['Flighttype']['title']]['duration'] = $stats['Flighttypes'][$flight['Flighttype']['title']]['duration']+$flight['Flight']['duration'];
 				
+				//LOCATIONS
 				$stats['Locations'][$flight['Location']['title']]['numbers'] = $stats['Locations'][$flight['Location']['title']]['numbers']+1;
 				$stats['Locations'][$flight['Location']['title']]['duration'] = $stats['Locations'][$flight['Location']['title']]['duration']+$flight['Flight']['duration'];
 				
+				//GLIDERS
 				$stats['Gliders'][$flight['Glider']['reg'].' - '.$flight['Glider']['type']]['numbers'] = $stats['Gliders'][$flight['Glider']['reg'].' - '.$flight['Glider']['type']]['numbers']+1;
 				$stats['Gliders'][$flight['Glider']['reg'].' - '.$flight['Glider']['type']]['duration'] = $stats['Gliders'][$flight['Glider']['reg'].' - '.$flight['Glider']['type']]['duration']+$flight['Flight']['duration'];
 				
+				//GLIDERTYPES
 				$stats['Glidertypes'][$flight['Glider']['type']]['numbers'] = $stats['Gliders'][$flight['Glider']['type']]['numbers']+1;
 				$stats['Glidertypes'][$flight['Glider']['type']]['duration'] = $stats['Gliders'][$flight['Glider']['type']]['duration']+$flight['Flight']['duration'];
-
+				
+				//MONTHS
+				$stats['Months'][date('Y-m',strtotime($flight['Flight']['start']))]['numbers'] = $stats['Months'][date('Y-m',strtotime($flight['Flight']['start']))]['numbers'] + 1;
+				$stats['Months'][date('Y-m',strtotime($flight['Flight']['start']))]['duration'] = $stats['Months'][date('Y-m',strtotime($flight['Flight']['start']))]['duration'] + $flight['Flight']['duration'];
 		}
 		
 		return $stats;
--- app/views/flights/statistics.ctp
+++ app/views/flights/statistics.ctp
@@ -1,4 +1,16 @@
+<!--[if lt IE 9]><script language="javascript" type="text/javascript" src="excanvas.js"></script><![endif]-->
+<script language="javascript" type="text/javascript" src="/js/libs/jquery.jqplot.min.js"></script>
+<link rel="stylesheet" type="text/css" href="/css/jquery.jqplot.css" />
+
 <div class="clear" style="height:15px;"></div>
+
+<div class="grid_12">
+<div class="box">
+<div id="chart"></div>
+</div>
+</div>
+
+<div class="clear"></div>
 <div class="grid_4">
 <div class="box">
 <h2>Types</h2>
@@ -127,3 +139,34 @@ foreach($stats['Locations'] as $title => $stat)
 </table>
 </div>
 </div>
+
+<script type="text/javascript">
+
+$.jqplot('chart',  
+[
+	[
+		[0,0],
+		<?php
+		$i = 0;
+		foreach($stats['Months'] as $title => $stat)
+		{	$i++;
+			echo '['.$i.','.$stat['duration'].']';
+			if($i!=count($stats['Months']))
+			{
+			echo ',';
+			}
+		}
+		
+		?>
+	],
+	[
+		[0,0],
+		[1,10]
+	]
+]
+, 
+ {
+   series:[{color:'#5f5'},{color:'#55f'}]
+ });
+
+</script>
